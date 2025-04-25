79a51aad0a6739724894b658a4c34a29565edcd2
AhrorSulaymonov <ahrorsulaymonov494@gmail.com>
2025-04-25 23:07:53 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import EditDocument from "@mui/icons-material/EditDocument";
 import EditDocumentIcon from "@mui/icons-material/EditDocument";
-import { Typography } from "antd";
 import UsersManagemand from "./components/UsersManagemand";
 function App() {
   return (
